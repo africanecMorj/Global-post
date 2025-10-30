14f880b46c6e8bd3325e8a24c035f8e56c403c96
Letfin <taratula2010@gmail.com>
2025-10-30 20:33:41 +0200

    Create Servise page

---
+++ app/components/Services.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Services.css'
+
+export default function Services() {
+  return (
+    <div className='Services_page'>
+    <div className='Services_title'>
+        Послуги, якими <span>ви можете</span> <br/> скористатися
+    </div>
+    
+    <div className='Services_buttons'></div>
+    </div>
+  )
+}
