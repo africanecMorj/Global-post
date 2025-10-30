585764d8f424c0a1e02f4430a92c6e75fe1ef000
Africanec Morj <transformatoratagac@gmail.com>
2025-10-30 18:06:22 +0000

    debuging file structure

---
+++ app/components/contactForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function contactForm() {
+  return (
+    <>
+        <div className='bg-amber-100 h-screen w-screen flex items-center justify-center'>
+                <div className='h-screen w-1/2 relative'>
+
+                </div>
+        </div>
+    </>
+  )
+}
